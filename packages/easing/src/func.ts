9944d688f808abe48c188eafde88015835d7d2b1
lihaochen <lihaochen202@outlook.com>
2023-04-18 12:12:43 +0800

    feat(easing): add `easeInCubicFunc`, `easeOutCubicFunc`, `easeInOutCubicFunc`

--- packages/easing/src/func.ts
+++ packages/easing/src/func.ts
@@ -39,6 +39,20 @@ export const easeInOutQuadFunc: EasingFunc = (t, b, c, d) => {
   return (-c / 2) * (--t * (t - 2) - 1) + b
 }
 
+export const easeInCubicFunc: EasingFunc = (t, b, c, d) => {
+  return c * (t /= d) * t * t + b
+}
+
+export const easeOutCubicFunc: EasingFunc = (t, b, c, d) => {
+  return c * ((t = t / d - 1) * t * t + 1) + b
+}
+
+export const easeInOutCubicFunc: EasingFunc = (t, b, c, d) => {
+  if ((t /= d / 2) < 1)
+    return (c / 2) * t * t * t + b
+  return (c / 2) * ((t -= 2) * t * t + 2) + b
+}
+
 export const easeInQuartFunc: EasingFunc = (t, b, c, d) => {
   return c * (t /= d) * t * t * t + b
 }
